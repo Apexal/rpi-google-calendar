8fb71df779c3aa9537ace23b23bd4bdde911cde0
Frank Matranga <thefrankmatranga@gmail.com>
2020-10-09 16:36:11 -0400

    Constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+PERIOD_TYPES = {
+    'LEC': 'Lecture',
+    'LAB': 'Lab',
+    'TES': 'Test',
+    'REC': 'Recitation',
+    'STU': 'Studio'
+}
+
+DAY_NAMES = ['Sunday', 'Monday', 'Tuesday',
+             'Wednesday', 'Thursday', 'Friday', 'Saturday']
+SHORT_DAY_NAMES = ['Sun', 'Mon', 'Tue',
+                   'Wed', 'Thu', 'Fri', 'Sat']
